1d5eab7cca56904b4b76d3ce48eda99ee7c5d1d2
shalihin313 <shalihinahmaddullah@gmail.com>
2024-08-14 01:49:49 +0800

    update query

--- code.php
+++ code.php
@@ -19,7 +19,7 @@ if (isset($_GET['add'])) {
     $title = $_GET['title'];
     $genre = $_GET['genre'];
     $director = $_GET['director'];
-    $release_date = $_GET['date']; // Adjusted variable name
+    $release_date = $_GET['date']; // Correct variable name
     $description = $_GET['description'];
     $rating = $_GET['rating'];
     $poster = $_GET['poster'];
@@ -78,7 +78,7 @@ if (isset($_POST['update'])) {
     $title = $_POST['title'];
     $genre = $_POST['genre'];
     $director = $_POST['director'];
-    $release_date = $_POST['date']; // Adjusted variable name
+    $release_date = $_POST['date']; // Correct variable name
     $description = $_POST['description'];
     $rating = $_POST['rating'];
     $poster = $_POST['poster'];

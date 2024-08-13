78c78592ffec9778c97295dbdb95fc041de97c46
shalihin313 <shalihinahmaddullah@gmail.com>
2024-08-14 00:56:05 +0800

    with json.lock
    please work

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shalihin313/csad_project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '02e6ef1ecdef434e3c965a2b7a7546de9b7ea530',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'shalihin313/csad_project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '02e6ef1ecdef434e3c965a2b7a7546de9b7ea530',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
